6ec3aecdea533d1b6aa5c2990661d454b259eca4
Neeraj Kumar <neeraj05rajput@gmail.com>
2020-11-04 18:21:53 +0530

    done

--- controller/user/hotelOverviewController.js
+++ controller/user/hotelOverviewController.js
@@ -13,11 +13,11 @@ const sliderModel = require('../../models/slider')
 
 const hotelPreview = async(req, res, next) => {
     try {
-        const { hotelId } = req.body
-        if(!hotelId) return res.status(404).json({status:false, msg: 'Please provide hotel id.'})
-        const fetch_hotelview = await hotelModel.findOne({_id: hotelId})
+        const { hotel_id } = req.body
+        if(!hotel_id) return res.status(404).json({status:false, msg: 'Please provide hotel id.'})
+        const fetch_hotelview = await hotelModel.findOne({_id: hotel_id})
         if(!fetch_hotelview) return res.status(404).json({status: false, msg:'The hotel not found.'})
-        return res.json({status: true, msg:'successfully getting', data: fetch_hotelview})
+        return res.status(200).json({status: true, msg:'successfully getting', data: fetch_hotelview})
     } catch (error) {
         console.log(error)
         return res.status(500).json({status:false, msg: 'something went wrong.'})
