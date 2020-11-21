a32a7ea5aebbbdf790c52a2bd6b22555b8dc7ce0
Neeraj Kumar <neeraj05rajput@gmail.com>
2020-11-21 23:33:38 +0530

    done aminities preview

--- controller/user/hotelOverviewController.js
+++ controller/user/hotelOverviewController.js
@@ -9,7 +9,6 @@ const path = require('path')
 const roomModel = require('../../models/room')
 const hotelModel = require('../../models/hotel')
 const sliderModel = require('../../models/slider');
-const hotel = require('../../models/hotel');
 const aminitiesModel = require('../../models/aminities')
 const hotelAminitiesModel = require('../../models/hotelAminities')
 
@@ -21,12 +20,63 @@ const hotelPreview = async(req, res, next) => {
         const fetch_hotelview = await hotelModel.findOne({_id: hotelId})
         if(!fetch_hotelview) return res.status(404).json({status: false, msg:'The hotel not found.'})
         const fetch_hotelAminities = await hotelAminitiesModel.aggregate([
+            
+            // {
+            //     $match: { hotelId: fetch_hotelview._id }
+            // },
             {
-                $match: { hotelId: fetch_hotelview._id }
-            }
+                $lookup: {
+                    from: "aminities",
+                    as: "aminitiesView",
+                    let: {
+                        aminitiesId: "$aminitiesId"
+                    },
+                    pipeline: [
+                        { $addFields: { _id: { $toString: "$_id" } } },
+                        {
+                            $match: {
+                                $expr: {
+                                    $eq: ["$_id", "$$aminitiesId"]
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $unwind: {
+                    path: "$aminitiesView",
+                    preserveNullAndEmptyArrays: true
+                }
+            },
+            // {
+            //     $lookup: {
+            //         from: "users",
+            //         let : {
+            //             userId: "$createdById"
+            //         },
+            //         as: "customer",
+            //         pipeline: [
+            //             { $addFields: { _id: { $toString: "$_id" } } },
+            //             {
+            //                 $match: {
+            //                     $expr: {
+            //                     $and: [{ $eq: ["$_id", "$$userId"] }]
+            //                     }
+            //                 }
+            //             }
+            //         ]
+            //     }
+            // },
+            // {
+            //     $addFields: {
+            //         customer: { $arrayElemAt: ["$customer", 0] }
+            //     }
+            // },
+            
         ])
-        // return res.json({data: fetch_hotelAminities})
-        return res.status(200).json({status: true, msg:'successfully getting', data: fetch_hotelview, aminities: fetch_hotelAminities, rules: JSON.parse(fetch_hotelview.rules), callus: JSON.parse(fetch_hotelview.callUs)})
+        return res.json({data: fetch_hotelAminities})
+        // return res.status(200).json({status: true, msg:'successfully getting', data: fetch_hotelview, aminities: fetch_hotelAminities, rules: JSON.parse(fetch_hotelview.rules), callus: JSON.parse(fetch_hotelview.callUs)})
         
     } catch (error) {
         console.log(error)
