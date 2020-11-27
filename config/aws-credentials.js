33ea0e815ae10143cafa94af9b627bb738bb34f5
Neeraj Kumar <neeraj05rajput@gmail.com>
2020-11-27 20:21:16 +0530

    DONE AWS

---
+++ config/aws-credentials.js
@@ -0,0 +1,8 @@
+const AWS = {
+    bucket: 'wondermiles-bucket',
+    aws_secret_access_key: '4DugweXFq9cIBLzdDTAdqI4zPCB86ExSObuUTWbU',
+    aws_access_key_id: 'AKIAISUWXMV5PO2IUXOA',
+    aws_region: 'us-east-2'
+}
+
+module.exports = AWS
